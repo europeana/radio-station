78bb8d1e26a2f10ec69a35bd18658714953813bc
Richard Doe <richard.doe@rwdit.net>
2016-08-23 11:25:43 +0100

    Random order for tracks

--- app/models/track.rb
+++ app/models/track.rb
@@ -6,4 +6,12 @@ class Track < ApplicationRecord
   belongs_to :tune
 
   validates :playlist_id, :tune_id, :order, presence: true
+  validates :order, uniqueness: { scope: :playlist_id }
+
+  before_validation do |track|
+    # Set a random track order, unique to this playlist
+    while track.order.blank? or (Track.where(playlist_id: track.playlist_id, order: track.order).count > 0)
+      track.order = rand(2147483647) unless track.order.present? # PG int max
+    end
+  end
 end
--- test/models/track_test.rb
+++ test/models/track_test.rb
@@ -7,4 +7,5 @@ class TrackTest < ActiveSupport::TestCase
   should validate_presence_of(:tune_id)
   should validate_presence_of(:playlist_id)
   should validate_presence_of(:order)
+  should validate_uniqueness_of(:order).scoped_to(:playlist_id)
 end
