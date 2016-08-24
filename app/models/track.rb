dd64141d71761f58e513bde22632cb5424ba5917
Richard Doe <richard.doe@rwdit.net>
2016-08-24 12:18:40 +0100

    Fix track order validation

--- app/models/track.rb
+++ app/models/track.rb
@@ -10,12 +10,16 @@ class Track < ApplicationRecord
   delegate :metadata, :thumbnail, :uri, :title, :creator, :europeana_record_id, to: :tune
 
   validates :playlist_id, :tune_id, :order, presence: true
+  validates :tune_id, uniqueness: { scope: :playlist_id }
   validates :order, uniqueness: { scope: :playlist_id }
 
   before_validation do |track|
     # Set a random track order, unique to this playlist
-    while track.order.blank? or (Track.where(playlist_id: track.playlist_id, order: track.order).count > 0)
+    while track.order.nil?
       track.order = rand(2147483647) # PG int max
+      if Track.where.not(id: track.id).where(playlist_id: track.playlist_id, order: track.order).count > 0
+        track.order = nil
+      end
     end
   end
 end
--- test/models/track_test.rb
+++ test/models/track_test.rb
@@ -6,5 +6,6 @@ class TrackTest < ActiveSupport::TestCase
   should belong_to(:playlist)
   should validate_presence_of(:tune_id)
   should validate_presence_of(:playlist_id)
+  should validate_uniqueness_of(:tune_id).scoped_to(:playlist_id)
   should validate_uniqueness_of(:order).scoped_to(:playlist_id)
 end
