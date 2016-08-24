e637995f039bf90b51b5adabf7e06da93699a0f8
Richard Doe <richard.doe@rwdit.net>
2016-08-24 09:38:55 +0100

    Set REDIS_URL on Pivotal

---
+++ deploy/pivotal/config/initializers/redis.rb
@@ -0,0 +1,9 @@
+if ENV.key?('VCAP_SERVICES')
+  services = JSON.parse(ENV['VCAP_SERVICES'])
+  redis_config = services['rediscloud'].first
+end
+
+fail 'No Redis service found in environment.' if redis_config.nil?
+
+redis_cred = redis_config['credentials']
+ENV['REDIS_URL'] = "redis://:#{redis_cred['password']}@#{redis_cred['hostname']}:#{redis_cred['port']}/0/jobs"
