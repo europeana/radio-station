621d111ccfa820958128b4b77e2ab3b3cd06225b
Richard Doe <richard.doe@rwdit.net>
2016-08-25 11:05:12 +0100

    Redirect root to stations list

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'stations#index'
+  root to: redirect('stations.json')
 
   resources :stations, only: %w(index show)
 end
