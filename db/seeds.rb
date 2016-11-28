7a20dd6a084217ecde65859516fa8ba25bde7472
Richard Doe <richard.doe@rwdit.net>
2016-11-28 10:55:23 +0000

    Change the API query for the folk station

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ unless Station.find_by_theme_type_and_slug(:genre, :folk)
   Station.create(
     name: 'Folk and Traditional Music',
     slug: 'folk',
-    api_query: '(europeana_collectionName:"282059213_Ag_EU_eSOUNDS_1013_ITMA" AND TYPE:"SOUND") OR (europeana_collectionName:"2059219_Ag_EU_eSOUNDS_1015_BNF" AND what:"musique traditionnelles") OR europeana_collectionName:"09335_Ag_EU_Judaica_cfmj4" OR europeana_collectionName:"2022420_Ag_RO_Elocal_cjcpctmu" OR europeana_collectionName:"2059206_Ag_EU_eSOUNDS_1011_FMS"',
+    api_query: 'DATA_PROVIDER:"Comhaltas Traditional Music Archive" OR DATA_PROVIDER:"LMTA (DIZI)" OR DATA_PROVIDER:"Irish Traditional Music Archive"',
     theme_type: :genre
   )
 end
