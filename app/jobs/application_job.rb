8ab7d63202344dacda59fcaf4d0f38ae7cf4b4fd
Richard Doe <richard.doe@rwdit.net>
2016-11-29 12:06:45 +0000

    Update generic station filters and genre queries

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class ApplicationJob < ActiveJob::Base
   def api_playable_audio_qf_params
-    %w(MEDIA:true SOUND_DURATION:medium SOUND_DURATION:long TYPE:SOUND)
+    %w(TYPE:SOUND provider_aggregation_edm_isShownBy:*)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,10 @@
 # frozen_string_literal: true
 # rubocop:disable Metrics/LineLength
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 unless Station.find_by_theme_type_and_slug(:genre, :classical)
   Station.create(
     name: 'Classical Music',
     slug: 'classical',
-    api_query: 'europeana_collectionName:"2048401_Ag_DE_DDB_eseslub" OR (europeana_collectionName:"2059219_Ag_EU_eSOUNDS_1015_BNF" AND what:"musique classique") OR (europeana_collectionName:"2059201_Ag_EU_eSOUNDS_1003_Latvia" AND what:(operas OR arranged))',
+    api_query: 'DATA_PROVIDER:("National Library of Latvia" OR "Sächsische Landesbibliothek - Staats- und Universitätsbibliothek Dresden")',
     theme_type: :genre
   )
 end
@@ -20,7 +13,7 @@ unless Station.find_by_theme_type_and_slug(:genre, :folk)
   Station.create(
     name: 'Folk and Traditional Music',
     slug: 'folk',
-    api_query: 'DATA_PROVIDER:"Comhaltas Traditional Music Archive" OR DATA_PROVIDER:"LMTA (DIZI)" OR DATA_PROVIDER:"Irish Traditional Music Archive"',
+    api_query: '(DATA_PROVIDER:("LMTA (DIZI)" OR "Comhaltas Traditional Music Archive" OR "Irish Traditional Music Archive" OR "Tobar an Dualchais/Kist o Riches" OR "Cluj County Centre for the Preservation and Promotion of Traditional Culture" OR "Bibliothèque Medem - Maison de la Culture Yiddish" OR "Music Library of Greece of The Friends of Music Society" OR "Statsbiblioteket")) AND (what:(music OR música OR musique OR musik OR musica OR musicales OR muziek OR muzyka OR muzică OR "zenés előadás" OR "notated music" OR "traditional and folk music" OR "folk songs" OR "sheet music" OR score OR "musical Instrument" OR partitur OR partituras OR gradual OR libretto OR oper OR concerto OR symphony OR sonata OR fugue OR motet OR estampie OR "gregorian chant" OR saltarello OR organum OR ballade OR chanson OR galliard OR laude OR madrigal OR pavane OR ricercar OR tiento OR toccata OR cantata OR chaconne OR gavotte OR gigue OR minuet OR partita OR passacaglia OR sarabande OR sinfonia OR hymnes OR lied OR mazurka OR "music hall" OR quartet OR quintet OR requiem OR rhapsody OR rondo OR scherzo OR "sinfonia concertante" OR waltz OR ballet OR zanger OR chanteur OR chanteuse OR sång OR sångare OR sänger OR cantante OR sopran OR tenor OR sängerin OR composer OR compositeur OR orchestra OR orchester OR orkester OR orchestre OR concert OR concierto OR konsert OR konzert OR koncert OR gramophone OR "record player" OR phonograph OR fonograaf OR fonograf OR grammofon OR skivspelare OR "wax cylinder" OR jukebox OR "cassette deck" OR "cassette player"))',
     theme_type: :genre
   )
 end
