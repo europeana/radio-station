b8bd5aac7dfa18a7c28eda988ade253e318e6cc4
Richard Doe <richard.doe@rwdit.net>
2016-12-01 14:26:34 +0000

    Scope uniqueness test

--- test/models/tune_test.rb
+++ test/models/tune_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 
 class TuneTest < ActiveSupport::TestCase
   should validate_presence_of(:web_resource_uri)
-  should validate_uniqueness_of(:web_resource_uri)
+  should validate_uniqueness_of(:web_resource_uri).scoped_to(:europeana_record_id)
   should belong_to(:origin)
   should have_many(:plays).dependent(:nullify)
   should have_and_belong_to_many(:stations)
