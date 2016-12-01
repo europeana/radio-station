ac643702b528c2a31eda1776bb62078381072062
Richard Doe <richard.doe@rwdit.net>
2016-12-01 10:38:01 +0000

    Do not try to generate playlists for stations with no tunes

--- app/jobs/generate_new_station_playlist_job.rb
+++ app/jobs/generate_new_station_playlist_job.rb
@@ -3,6 +3,8 @@ class GenerateNewStationPlaylistJob < ApplicationJob
   queue_as :playlist_generation
 
   def perform(station_id)
+    station = Station.find_by_id(station_id)
+    return if station.nil? || station.tunes.count.zero?
     Playlist.create(station_id: station_id).live!
   end
 end
